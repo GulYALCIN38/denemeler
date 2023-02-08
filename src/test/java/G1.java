27c13762439f2b4f1e75441c9d484cece24817de
pmelifge <109210047+pmelifge@users.noreply.github.com>
2023-02-08 20:58:34 +0200

    v1

--- src/test/java/G1.java
+++ src/test/java/G1.java
@@ -1,5 +1,9 @@
+import java.io.PrintStream;
+
 public class G1 {
     public static void main(String[] args) {
         System.out.println("merhaba");
+        System.out.println("hello world");
+
     }
 }
